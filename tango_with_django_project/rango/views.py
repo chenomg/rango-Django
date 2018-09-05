c829d8960f6488d4c4e8e81c5e646081eeee6884
Jase Chen <xxmm@live.cn>
2018-09-06 00:02:11 +0800

    _

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse('Rango says hey there partner<p>Click <a href="about/">Here</a> go to about!</p>')
+
+
+def about(request):
+    return HttpResponse('Rango says here is the about page!<p>Click <a href="../">Here</a> back to home!</p>')
