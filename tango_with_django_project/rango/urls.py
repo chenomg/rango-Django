eddde6ea9f2c6ed909c53b28b2c0d0882618cd8b
Jase Chen <xxmm@live.cn>
2018-09-14 00:27:19 +0800

    _

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # encoding: utf-8
 
 from django.conf.urls import url
@@ -6,6 +6,8 @@ from rango import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^about/', views.about, name='about'),
+    url(r'^about/$', views.about, name='about'),
+    url(r'^add_category/$', views.add_category, name='add_category'),
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.show_category, name='show_category'),
 ]
