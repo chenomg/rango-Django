1abf399a00e6e8bbd7b28c8a49262d136b487789
Jase Chen <xxmm@live.cn>
2018-10-15 23:19:51 +0800

    _

--- tango_with_django_project/rango/tests.py
+++ tango_with_django_project/rango/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 from rango.models import Category
 
 
@@ -17,3 +18,34 @@ class CategoryMethodTest(TestCase):
         cat = Category(name='test', views=-1, likes=0)
         cat.save()
         self.assertEqual((cat.views >= 0), True)
+
+    def test_slug_line_creation(self):
+        cat = Category(name='Random Category String')
+        cat.save()
+        self.assertEqual(cat.slug, 'random-category-string')
+
+
+class IndexViewTest(TestCase):
+    def test_index_view_with_no_categories(self):
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'There are no categories present.')
+        self.assertQuerysetEqual(response.context['categories'], [])
+
+    def add_cat(self, name, views, likes):
+        c = Category.objects.get_or_create(name=name)[0]
+        c.views = views
+        c.likes = likes
+        c.save()
+        return c
+
+    def test_index_view_with_categories(self):
+        self.add_cat('test', 1, 1)
+        self.add_cat('temp', 1, 1)
+        self.add_cat('tmp', 1, 1)
+        self.add_cat('tmp test temp', 1, 1)
+        response = self.client.get(reverse('rango:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'tmp test temp')
+        num_cats = len(response.context['categories'])
+        self.assertEqual(num_cats, 4)
